c494f8ab3dd9bf4dbef813fdb0e9822ea7fbde7d
Undermord <106814759+Undermord@users.noreply.github.com>
2024-02-18 20:13:36 +0400

    working with the Service class

--- app/Services/Post/Service.php
+++ app/Services/Post/Service.php
@@ -2,7 +2,26 @@
 
 namespace App\Services\Post;
 
-class PostService
-{
+use App\Models\Post;
 
+class Service
+{
+    public function store ($data)
+    {
+        $tags = $data['tags'];
+        unset($data['tags']);
+
+        $post = Post::create($data);
+        $post->tags()->attach($tags);
+
+    }
+    public function update ($post , $data)
+    {
+        $tags = $data['tags'];
+        unset($data['tags']);
+
+        $post->update($data);
+        $post->tags()->sync($tags);
+
+    }
 }
