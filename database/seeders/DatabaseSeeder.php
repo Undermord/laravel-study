2db24ee2f5b88148d9d9de198cf8a157bfbb24a6
Undermord <106814759+Undermord@users.noreply.github.com>
2024-02-24 20:38:56 +0400

    Изучение Factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\Category;
 use App\Models\Post;
+use App\Models\Tag;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -15,7 +16,16 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $posts =Post::factory(100)->create();
+        Category::factory(20)->create();
+        $tags = Tag::factory(40)->create();
+        $posts = Post::factory(150)->create();
+
+        foreach ($posts as $post)
+        {
+            $tagsId = $tags->random(5)->pluck('id');
+            $post -> tags()->attach($tagsId);
+        }
+
 
         // User::factory(10)->create();
     }
