b1981c0892c1631d0f225796af5576be887116ec
Undermord <106814759+Undermord@users.noreply.github.com>
2024-03-06 00:21:54 +0400

    Работа с Filter

---
+++ app/Http/Filters/PostFilter.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Filters;
+
+class PostFilter
+{
+    public const TITLE = 'title';
+    public const CONTENT = 'content';
+    public const CATEGORY_ID = 'category_id';
+}
